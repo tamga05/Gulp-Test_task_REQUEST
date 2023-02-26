d349652a09d2919733e828d78446c5cdc23729ae
tamga05 <tamga05@gmail.com>
2023-02-27 00:13:17 +0300

    Update 2023-02-26T21:13:14.248Z

--- src/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('click', function (e) {
-    let m = document.querySelector('.view');
-    if (e.target.id !== 'box') {
-        m.style.opacity = '0';
-    } else if (e.target.id === 'box') {
-        m.style.opacity = (m.style.opacity !== '1') ? '1' : '0';
-    }
-});
